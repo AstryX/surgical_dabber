1f2b2c9e31033b929db36fd69663eda1e4754bec
AstryX <robertasdere@gmail.com>
2020-07-09 22:31:06 +0100

    Rescaled point clouds to -1 1 pos xy ranges and 0-1 depth

--- src/CloudCreator.py
+++ src/CloudCreator.py
@@ -18,14 +18,18 @@ f=open("before.PCD", "a+")
 for i in range(len(before)):
     for j in range(len(before[i])):
         cur_colour = int(before[i][j][0]) + int(before[i][j][1]) + int(before[i][j][2])
-        cur_depth = max_colour - cur_colour
-        f.write(str(i) + " " + str(j) + " " + str(cur_depth) + " " + str(struct.unpack('<f',bytearray([colour[i][j][0],colour[i][j][1],colour[i][j][2],0]))[0]) + "\n")
+        cur_depth = 1 - cur_colour / max_colour
+        new_i = (i / height) * 2 - 1.0
+        new_j = (j / width) * 2 - 1.0
+        f.write(str(new_i) + " " + str(new_j) + " " + str(cur_depth) + " " + str(struct.unpack('<f',bytearray([colour[i][j][2],colour[i][j][1],colour[i][j][0],0]))[0]) + "\n")
 f.close()
 
 f=open("after.PCD", "a+")
 for i in range(len(after)):
     for j in range(len(after[i])):
         cur_colour = int(after[i][j][0]) + int(after[i][j][1]) + int(after[i][j][2])
-        cur_depth = max_colour - cur_colour
-        f.write(str(i) + " " + str(j) + " " + str(cur_depth) + " " + str(struct.unpack('<f',bytearray([colour[i][j][0],colour[i][j][1],colour[i][j][2],0]))[0]) + "\n")
+        cur_depth = 1 - cur_colour / max_colour
+        new_i = (i / height) * 2 - 1.0
+        new_j = (j / width) * 2 - 1.0
+        f.write(str(new_i) + " " + str(new_j) + " " + str(cur_depth) + " " + str(struct.unpack('<f',bytearray([colour[i][j][2],colour[i][j][1],colour[i][j][0],0]))[0]) + "\n")
 f.close()
