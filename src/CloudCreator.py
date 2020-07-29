c7e55e498a3cecbe6310950b1f04af6787a1a15e
AstryX <robertasdere@gmail.com>
2020-07-29 17:35:35 +0100

    Fixed cloud creator bug for python 2.7

--- src/CloudCreator.py
+++ src/CloudCreator.py
@@ -10,17 +10,17 @@ before = cv2.cvtColor(before, cv2.COLOR_BGR2RGB);
 after = cv2.cvtColor(after, cv2.COLOR_BGR2RGB); 
 colour = cv2.cvtColor(colour, cv2.COLOR_BGR2RGB); 
 
-width = 330
-height = 250
-max_colour = 255*3
+width = 330.0
+height = 250.0
+max_colour = 255*3.0
 
 f=open("before_3.PCD", "a+")
 for i in range(len(before)):
     for j in range(len(before[i])):
         cur_colour = int(before[i][j][0]) + int(before[i][j][1]) + int(before[i][j][2])
         cur_depth = 1 - cur_colour / max_colour
-        new_i = (i / height) * 2 - 1.0
-        new_j = (j / width) * 2 - 1.0
+        new_i = (float(i) / height) * 2 - 1.0
+        new_j = (float(j) / width) * 2 - 1.0
         f.write(str(new_i) + " " + str(new_j) + " " + str(cur_depth) + " " + str(struct.unpack('<f',bytearray([colour[i][j][2],colour[i][j][1],colour[i][j][0],0]))[0]) + "\n")
 f.close()
 
@@ -29,7 +29,7 @@ for i in range(len(after)):
     for j in range(len(after[i])):
         cur_colour = int(after[i][j][0]) + int(after[i][j][1]) + int(after[i][j][2])
         cur_depth = 1 - cur_colour / max_colour
-        new_i = (i / height) * 2 - 1.0
-        new_j = (j / width) * 2 - 1.0
+        new_i = (float(i) / height) * 2 - 1.0
+        new_j = (float(j) / width) * 2 - 1.0
         f.write(str(new_i) + " " + str(new_j) + " " + str(cur_depth) + " " + str(struct.unpack('<f',bytearray([colour[i][j][2],colour[i][j][1],colour[i][j][0],0]))[0]) + "\n")
 f.close()
